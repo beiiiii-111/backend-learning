f3bf3814f9c8446c4c15192eb37d44ba31c28b7a
zxyi <705551086@qq.com>
2026-09-16 08:32:25 +0800

    feat: 新增 03-logging 模块源码与依赖配置

--- 01-quickstart/src/main/java/top/yixz/config/quickstart/controller/UserController.java
+++ 01-quickstart/src/main/java/top/yixz/config/quickstart/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
+import java.util.List;
 
 /**
  * @author yxzhang
@@ -15,8 +16,28 @@ import java.time.LocalDate;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
+    /**
+     * 获取用户信息
+     *
+     * @return 用户信息
+     */
     @GetMapping("/info")
-    public User getUserInfo(){
-        return new User(1000L,"张三",LocalDate.of(2000,2,4));
+    public User getUserInfo() {
+        return new User(1000L, "张三", LocalDate.of(2000, 2, 4));
+    }
+
+    /**
+     * 查询用户列表
+     *
+     * @return 全部用户
+     */
+    @GetMapping("/list")
+    public List<User> listUsers() {
+        return List.of(
+                new User(1000L, "张三", LocalDate.of(2000, 2, 4)),
+                new User(1001L, "李四", LocalDate.of(1999, 8, 15)),
+                new User(1002L, "王五", LocalDate.of(2001, 6, 30))
+        );
     }
 }
